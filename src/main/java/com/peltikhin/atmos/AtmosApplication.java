cd8d10d48aecf48e13793febae9d18c8372858fb
Vlad <peltikhin.vladik@gmail.com>
2022-09-21 16:52:15 +0300

    Add files from Spring Initializr to initialize project

---
+++ src/main/java/com/peltikhin/atmos/AtmosApplication.java
@@ -0,0 +1,13 @@
+package com.peltikhin.atmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peltikhin/atmos/AtmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peltikhin.atmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
